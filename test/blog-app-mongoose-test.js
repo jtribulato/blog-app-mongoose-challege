a1bdd4805d695b086c7335147a2019f290b3e979
John Tribulato <jtribulato@gmail.com>
2019-01-16 17:18:58 -0800

    starting to edit

--- test/blog-app-mongoose-test.js
+++ test/blog-app-mongoose-test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const express = require('express');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const faker = require('faker');
@@ -13,3 +14,34 @@ const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
 
+
+//***** Funtion to Seed the Database  ******/
+function seedBlogData() {
+    console.info('seeding blog data');
+    const seedData = [];
+  
+    for (let i=1; i<=10; i++) {
+      seedData.push(generateBlogData());
+    }
+    console.log("Done with Push");
+    // this will return a promise
+    return BlogPost.insertMany(seedData);
+  }
+
+  
+
+
+
+  function generateBlogData() {
+    return {
+        title: faker.company.catchPhrase(),
+        author: 
+            {
+               firstName : faker.name.firstName(),
+               lastName : faker.name.lastName(),
+            },
+        content: faker.lorem.sentences()
+    };
+  }
+  
+  //console.log(generateBlogData()) ;
